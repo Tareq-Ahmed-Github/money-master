db10676c61ad5ca6c5d488fec75ca5f97572c867
unknown <tareqrobin57@gmail.com>
2022-02-18 22:48:33 +0600

    savings part also done

--- js/money-calculate.js
+++ js/money-calculate.js
@@ -1,15 +1,22 @@
+// function for calculate button click
+
 document.getElementById('calculate-btn').addEventListener('click', function () {
+
     // // storing income input
+
     const incomeInput = document.getElementById('income-input');
     const incomeAmount = incomeInput.value;
 
     // //expense input segment
+
     // food
     const foodExpenseInput = document.getElementById('food-expense-input');
     const foodExpenseAmount = foodExpenseInput.value;
+
     // rent
     const rentExpenseInput = document.getElementById('rent-expense-input');
     const rentExpenseAmount = rentExpenseInput.value;
+
     // clothes
     const clothesExpenseInput = document.getElementById('clothes-expense-input');
     const clothesExpenseAmount = clothesExpenseInput.value;
@@ -18,15 +25,17 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const totalExpenseAmount = parseFloat(foodExpenseAmount) + parseFloat(rentExpenseAmount) + parseFloat(clothesExpenseAmount);
 
     //show amount
+
     // total expense
     const totalExpense = document.getElementById('total-expense');
     totalExpense.innerText = /*parseFloat(totalExpense.innerText) + */ totalExpenseAmount;
+
     // balance
     const balanceTotal = document.getElementById('balance');
     balanceTotal.innerText = incomeAmount - totalExpense.innerText;
 
     // clear ipnut box 
-    incomeInput.value = '';
+    // incomeInput.value = '';
     foodExpenseInput.value = '';
     rentExpenseInput.value = '';
     clothesExpenseInput.value = '';
@@ -34,3 +43,29 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
 
 })
 
+
+// function for save button click
+
+document.getElementById('save-btn').addEventListener('click', function () {
+    //storing save input
+    const saveInput = document.getElementById('save-input');
+    const saveAmount = saveInput.value;
+
+    // // storing income input
+    const incomeInput = document.getElementById('income-input');
+    const incomeAmount = incomeInput.value;
+
+    //show saving amount
+
+    //saving amount
+    const savingAmount = document.getElementById('saving-amount');
+    savingAmount.innerText = parseFloat(saveAmount) * incomeAmount / 100;
+
+    //ramaining amount
+    const remainingAmount = document.getElementById('remaining-amount');
+    remainingAmount.innerText = incomeAmount - savingAmount.innerText;
+
+    // clear input box 
+    incomeInput.value = '';
+
+})
